8686c9586dd2fbaa56b2b2c8127b30724b5f6c77
colin <colin_bao@163.com>
2022-11-13 06:04:12 +0800

    Update

--- new_test/test.py
+++ new_test/test.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import multiprocessing as mp
+import numpy as np
+
+
 class DownLoader:
     DOWNLOAD_PATH_ROOT = f'/Users/mac/Downloads/load_img/'
 
@@ -9,45 +14,67 @@ class DownLoader:
 
     def down_by_gzh(self, biz='MjM5MzMwNjM0MA=='):
         """
-        按照公众号名称,分片下载
+        按照公众号名称,分片下载\n
         :param biz:
         :return:
         """
-        import pandas as pd
-        import requests
-        from tqdm.auto import tqdm
-        import os
 
         # 提取
-        self.df_select = pd.read_sql(
-            "SELECT id,cover,cover_local FROM articles_copy1 "
-            "WHERE biz=:biz AND mov=:mov AND p_date BETWEEN :sd AND :ed  AND cover_local IS NULL ",
-            con=self.engine, params={'biz': biz,
-                                     'sd': int(pd.to_datetime('20200101').timestamp()),
-                                     'ed': int(pd.to_datetime('20210101').timestamp()),
-                                     'mov': 10},
-            parse_dates=["p_date"], )
+        def extract():
+            self.df_select = pd.read_sql(
+                "SELECT id,cover,cover_local FROM articles_copy1 "
+                "WHERE biz=:biz AND mov=:mov AND p_date BETWEEN :sd AND :ed  AND cover_local IS NULL",
+                con=self.engine, params={'biz': biz,
+                                         'sd': int(pd.to_datetime('20200101').timestamp()),
+                                         'ed': int(pd.to_datetime('20210101').timestamp()),
+                                         'mov': 10},
+                parse_dates=["p_date"], )
+            return self.df_select
 
-        # 下载图片
-        load_path = self.DOWNLOAD_PATH_ROOT + f'{biz}/'
-        os.makedirs(load_path, exist_ok=True)
+        def down(df_extract):
+            import requests
+            import os
 
-        def down_url(x):
-            with open(load_path + f"{x['id']}.jpeg", 'wb') as f:
-                if f.write(requests.get(x['cover'], stream=True).content):
-                    return load_path + f"{x['id']}.jpeg"
-            return None
+            # 下载图片
+            load_path = self.DOWNLOAD_PATH_ROOT + f'{biz}/'
+            os.makedirs(load_path, exist_ok=True)
 
-        tqdm.pandas(desc=f'DownLoad IMG By {biz}')
-        self.df_select['cover_local'] = self.df_select[['id', 'cover']].progress_apply(lambda x: down_url(x), axis=1)
-        self.df_select.to_sql('temp', self.engine, index=False, if_exists='replace')
+            # self.df_select['cover_local'] = self.df_select[['id', 'cover']].progress_apply(lambda x: down_url(x),
+            # 下载图片
+            def down_url(x):
+                with open(load_path + f"{x['id']}.jpeg", 'wb') as f:
+                    if f.write(requests.get(x['cover'], stream=True).content):
+                        return load_path + f"{x['cover']}.jpeg"
 
+            import dask.dataframe as dd
+            from dask.diagnostics import ProgressBar
+            ddata = dd.from_pandas(df_extract, npartitions=5)
+            with ProgressBar():
+                df_extract['cover_local'] = ddata.map_partitions(lambda df: df.apply(lambda row: down_url(row), axis=1),
+                                                                 meta=df_extract.dtypes).compute(scheduler='processes')
+
+            return df_extract
+
+        down(extract())
         # 更新下载完的图片
-        sql = """
-        UPDATE articles_copy1 AS tar
-        SET cover_local = (SELECT temp.cover_local FROM temp WHERE temp.id = tar.id)
-        WHERE EXISTS(SELECT id,cover_local FROM temp WHERE temp.id = tar.id)
+        # self.update_by_temp(self.df_select, 'articles_copy1', 'cover_local', 'id')
+
+    def update_by_temp(self, df_temp: pd.DataFrame, update_table, update_column, update_pk):
         """
+        生成中间表来更新\n
+        :param df_temp:
+        :param update_table:
+        :param update_column:
+        :param update_pk:
+        :return:
+        """
+        update_table_temp = update_table + '_temp'
+        df_temp.to_sql(update_table_temp, self.engine, index=False, if_exists='replace')
+        sql = f"""
+                UPDATE {update_table} AS tar
+                SET {update_column} = (SELECT temp.{update_column} FROM {update_table_temp} AS temp WHERE temp.{update_pk} = tar.{update_pk})
+                WHERE EXISTS(SELECT {update_pk},{update_column} FROM {update_table_temp} AS temp WHERE temp.{update_pk} = tar.{update_pk})
+                """
         self.engine.execute(sql)
 
     def __enter__(self):
@@ -57,8 +84,11 @@ class DownLoader:
         self.engine.dispose()
 
 
+if __name__ == '__main__':
+    with DownLoader() as DownLoader:
+        DownLoader.down_by_gzh()
+
 # DownLoader().down_by_gzh()
-with DownLoader() as DownLoader:
-    DownLoader.down_by_gzh()
+
 
 #     DownLoader.down_by_gzh()
